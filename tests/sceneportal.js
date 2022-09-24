667c0bec2866ba134bd538ce9e63749b24993b21
_earthtospace <niema.widaha@gmail.com>
2022-09-24 14:29:47 -0700

    day 4/sec 1 done

--- tests/sceneportal.js
+++ tests/sceneportal.js
@@ -29,14 +29,16 @@ const main = async () => {
   let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
   console.log('👀 GIF Count', account.totalGifs.toString())
 
-  await program.rpc.addGif({
+  await program.rpc.addGif("https://media.giphy.com/media/ejJclNX60XyEo555wW/giphy.gif", {
     accounts: {
       baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
     },
   });
 
   account = await program.account.baseAccount.fetch(baseAccount.publicKey);
-  console.log('👀 GIF Count', account.totalGifs.toString())
+  console.log('👀 GIF Count', account.totalGifs.toString());
+  console.log('👀 GIF List', account.gifList);
 }
 
 const runMain = async () => {
